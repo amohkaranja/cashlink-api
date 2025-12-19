e90937fb0672047e7441d7698f13b119de1c99c1
Amos Ngigi <40180931+amohkaranja@users.noreply.github.com>
2025-12-19 15:00:44 +0300

    multi- platform

--- CashLink.Tests/obj/Debug/net9.0/CashLink.Tests.AssemblyInfo.cs
+++ CashLink.Tests/obj/Debug/net9.0/CashLink.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashLink.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cb181f62827cf40ef650a55a82cd35fb675c02a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashLink.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashLink.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
